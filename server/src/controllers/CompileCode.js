b786c51ba0476b17fee4012c3b2f89eeebb73e80
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-11-16 12:10:18 +0530

    Fixing compiler error

--- server/src/controllers/CompileCode.js
+++ server/src/controllers/CompileCode.js
@@ -1,5 +1,6 @@
 const { exec } = require('child_process');
 const fs = require("fs");
+const path = require("path");
 
 const CompileCode = async (req, res) => {
   const { code, language, testCases } = req.body;
@@ -8,17 +9,15 @@ const CompileCode = async (req, res) => {
     return res.status(400).json({ error: "Code or language field is missing" });
   }
 
+  const extension = getExtension(language);
+  const fileName = `/tmp/temp${extension}`;
+
   try {
-    const extension = getExtension(language);
-    const fileName = `temp${extension}`;
-    
     fs.writeFileSync(fileName, code);
 
     const command = getCommand(language, fileName);
     const input = testCases;
-
     const childProcess = exec(command, { timeout: 5000 }, (error, stdout, stderr) => {
-      fs.unlinkSync(fileName);
       if (error) {
         return res.status(400).json({ error: stderr || error.message });
       }
@@ -26,35 +25,37 @@ const CompileCode = async (req, res) => {
     });
 
     if (input) {
-      childProcess.stdin.write(input+"\n");
+      childProcess.stdin.write(input + "\n");
     }
     childProcess.stdin.end();
 
   } catch (error) {
     console.error("Internal server error:", error);
-    res.status(500).json({ error: error });
+    res.status(500).json({ error: "Internal server error" });
+  } finally {
+    if (fs.existsSync(fileName)) fs.unlinkSync(fileName);
   }
 };
 
 const getCommand = (language, fileName) => {
   switch (language) {
     case "cpp":
-      return `g++ ${fileName} -o temp && ./temp`;
+      return `g++ ${fileName} -o /tmp/temp && /tmp/temp`;
     case "python":
       return `python3 ${fileName}`;
     case "c":
-      return `gcc ${fileName} -o temp && ./temp`;
+      return `gcc ${fileName} -o /tmp/temp && /tmp/temp`;
     default:
-      throw new Error("Unsupported language");
+      throw new Error(`${language} is not supported at this time.`);
   }
 };
 
 const getExtension = (language) => {
   switch (language) {
-    case 'cpp': return '.cpp';
-    case 'python': return '.py';
-    case 'c': return '.c'
-    default: throw new Error("Unsupported language");
+    case "cpp": return ".cpp";
+    case "python": return ".py";
+    case "c": return ".c";
+    default: throw new Error(`${language} is not supported at this time.`);
   }
 };
 
