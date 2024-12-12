d5bafcb09b738cb72ff095a82a120199878bca0f
SameerVohra <sameervohra943@gmail.com>
2024-12-12 13:19:43 +0530

    Fixing c++ compiler issue

--- server/src/controllers/CompileCode.js
+++ server/src/controllers/CompileCode.js
@@ -1,47 +1,46 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 const fs = require("fs");
+const path = require("path");
 
 const CompileCode = async (req, res) => {
   const { code, language, testCases } = req.body;
-  
+
   if (!code || !language) {
     return res.status(400).json({ error: "Code or language field is missing" });
   }
 
   try {
     const extension = getExtension(language);
-    const fileName = `temp${extension}`;
-    
-    fs.writeFileSync(fileName, code);
+    const fileName = `temp_${Date.now()}${extension}`;
+    const filePath = path.resolve(__dirname, fileName);
 
-    const command = getCommand(language, fileName);
-    const input = testCases;
+    fs.writeFileSync(filePath, code);
+
+    const command = getCommand(language, filePath);
 
     const childProcess = exec(command, { timeout: 5000 }, (error, stdout, stderr) => {
-      fs.unlinkSync(fileName);
+      fs.unlinkSync(filePath);
       if (error) {
         return res.status(400).json({ error: stderr || error.message });
       }
       res.json({ output: stdout });
     });
 
-    if (input) {
-      childProcess.stdin.write(input+"\n");
+    if (testCases) {
+      childProcess.stdin.write(testCases + "\n");
     }
     childProcess.stdin.end();
-
   } catch (error) {
-    console.error("Internal server error:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
-const getCommand = (language, fileName) => {
+const getCommand = (language, filePath) => {
   switch (language) {
     case "cpp":
-      return `g++ ${fileName} -o temp && ./temp`;
+      return `g++ ${filePath} -o ${filePath}.out && ${filePath}.out`;
     case "python":
-      return `python3 ${fileName}`;
+      return `python3 ${filePath}`;
     default:
       throw new Error("Unsupported language");
   }
@@ -49,8 +48,8 @@ const getCommand = (language, fileName) => {
 
 const getExtension = (language) => {
   switch (language) {
-    case 'cpp': return '.cpp';
-    case 'python': return '.py';
+    case "cpp": return ".cpp";
+    case "python": return ".py";
     default: throw new Error("Unsupported language");
   }
 };
