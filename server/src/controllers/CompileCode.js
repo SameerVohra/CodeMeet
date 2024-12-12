a37c5c0a942c12f15edf480f6563a022d5f9e0ca
SameerVohra <sameervohra943@gmail.com>
2024-12-12 13:28:19 +0530

    Fixing compiler

--- server/src/controllers/CompileCode.js
+++ server/src/controllers/CompileCode.js
@@ -1,6 +1,5 @@
-const { exec } = require("child_process");
+const { exec } = require('child_process');
 const fs = require("fs");
-const path = require("path");
 
 const CompileCode = async (req, res) => {
   const { code, language, testCases } = req.body;
@@ -11,36 +10,38 @@ const CompileCode = async (req, res) => {
 
   try {
     const extension = getExtension(language);
-    const fileName = `temp_${Date.now()}${extension}`;
-    const filePath = path.resolve(__dirname, fileName);
+    const fileName = `temp${extension}`;
 
-    fs.writeFileSync(filePath, code);
+    fs.writeFileSync(fileName, code);
 
-    const command = getCommand(language, filePath);
+    const command = getCommand(language, fileName);
+    const input = testCases;
 
     const childProcess = exec(command, { timeout: 5000 }, (error, stdout, stderr) => {
-      fs.unlinkSync(filePath);
+      fs.unlinkSync(fileName);
       if (error) {
         return res.status(400).json({ error: stderr || error.message });
       }
       res.json({ output: stdout });
     });
 
-    if (testCases) {
-      childProcess.stdin.write(testCases + "\n");
+    if (input) {
+      childProcess.stdin.write(input + "\n");
     }
     childProcess.stdin.end();
+
   } catch (error) {
+    console.error("Internal server error:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
-const getCommand = (language, filePath) => {
+const getCommand = (language, fileName) => {
   switch (language) {
     case "cpp":
-      return `g++ ${filePath} -o ${filePath}.out && ${filePath}.out`;
+      return `g++ ${fileName} -o temp && ./temp`;
     case "python":
-      return `python3 ${filePath}`;
+      return `python3 ${fileName}`;
     default:
       throw new Error("Unsupported language");
   }
@@ -48,8 +49,8 @@ const getCommand = (language, filePath) => {
 
 const getExtension = (language) => {
   switch (language) {
-    case "cpp": return ".cpp";
-    case "python": return ".py";
+    case 'cpp': return '.cpp';
+    case 'python': return '.py';
     default: throw new Error("Unsupported language");
   }
 };
